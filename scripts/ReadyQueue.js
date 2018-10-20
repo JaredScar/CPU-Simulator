cf7906ac954123975f5052024b41ebea470038a3
TheWolfBadger <TheWolfBadger@gmail.com>
2018-10-20 16:57:17 -0400

    - All algorithms complete and functioning correctly!
    [STILL TO DO]
    - More comments within the project
    [BUGS]
    - Round-Robin not doing jobs in queue in order from first come first served

--- scripts/ReadyQueue.js
+++ scripts/ReadyQueue.js
@@ -128,11 +128,12 @@ class ReadyQueue {
     getRR(currentClockTick, maxQuantums) {
         var jobFinal = this.getFirstCome();
         var quantCount = jobFinal.getQuantCount();
+        console.log("FirstCome from RR is: Job" + jobFinal.getPID()); // TODO debug, get rid of
         for(var i=0; i < this.queue.length; i++) {
             var job = this.queue[i];
             if(job.getArrivalTime() <= currentClockTick) {
-                if(jobFinal.getQuantCount() >= maxQuantums) {
-                    if(jobFinal.getTimesActive() > job.getTimesActive()) {
+                if((jobFinal.getQuantCount() % maxQuantums) == 0) {
+                    if (jobFinal.getTimesActive() > job.getTimesActive()) {
                         jobFinal = job;
                         quantCount = job.getQuantCount();
                     }
